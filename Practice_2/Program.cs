c13c333697b1f89ca21ed91526bb09ebead65a88
mirpro2001 <70947376+mirpro2001@users.noreply.github.com>
2020-09-13 18:05:26 +0300

    Add files via upload

---
+++ Practice_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PR02_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Microsoft .NET Framework 2 Application Development Foundation";
+            string[] sa = s.Split(" ");
+            Array.Sort(sa);
+            s = string.Join(" ", sa);
+            Console.WriteLine(s);
+        }
+    }
+}
