95f119fba71ad630d008becb0f020038c0f2e2a5
mirpro2001 <70947376+mirpro2001@users.noreply.github.com>
2020-09-21 19:29:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace CompressionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inFilename = @"d:\ttt.txt";
+            string outFilename = @"d:\ttt.txt.gz";
+            string inFilename1 = @"d:\tttt.txt";
+            FileStream sourceFile = File.OpenRead(outFilename);
+            FileStream destFile = File.Create(inFilename1);
+            GZipStream compStream =
+            new GZipStream(sourceFile, CompressionMode.Decompress);
+            int theByte = compStream.ReadByte();
+            while (theByte != -1)
+            {
+                destFile.WriteByte((byte)theByte);
+                theByte = compStream.ReadByte();
+            }
+
+            compStream.Close();
+
+
+        }
+    }
+}
