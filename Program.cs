85ff5e90ce39bb1dc54092d2774e163ff0c223c8
mirpro2001 <70947376+mirpro2001@users.noreply.github.com>
2020-09-21 19:15:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+namespace FileWatchingDemo
+{
+    class Program
+    {
+        static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Changed: {0}", e.FullPath);
+        }
+
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher =
+            new FileSystemWatcher(@"C:\Documents");
+            watcher.Filter = "*.*";
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+            watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+            Console.ReadKey();
+        }
+    }
+}
