1693a90768b872f44a99e7d12e6a9a309116e438
mirpro2001 <70947376+mirpro2001@users.noreply.github.com>
2020-09-22 14:45:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+namespace SequentialCollections
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Queue queue = new Queue();
+
+      queue.Enqueue("ARTEM");
+      queue.Enqueue("DIMA");
+      queue.Enqueue("LOVE C++");
+      queue.Enqueue("***");
+
+      while (queue.Count > 0)
+      {
+        object obj = queue.Dequeue();
+        Console.WriteLine("From Queue: {0}", obj);
+      }
+    }
+  }
+}
