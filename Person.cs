0e570d79dae3c8ab175f1312920127e8f02f20fe
mirpro2001 <70947376+mirpro2001@users.noreply.github.com>
2020-10-06 13:34:45 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+
+namespace Serialize_People
+{
+  [Serializable]
+  class Person : IDeserializationCallback
+  {
+    public string name;
+    public DateTime dateOfBirth;
+    [NonSerialized] public int age;
+
+    public Person(string _name, DateTime _dateOfBirth)
+    {
+      name = _name;
+      dateOfBirth = _dateOfBirth;
+      CalculateAge();
+    }
+
+    public Person()
+    {
+    }
+
+    public override string ToString()
+    {
+      return name + " was born on " + dateOfBirth.ToShortDateString() + " and is " + age.ToString() + " years old.";
+    }
+
+    private void CalculateAge()
+    {
+      age = DateTime.Now.Year - dateOfBirth.Year;
+      if (dateOfBirth.AddYears(DateTime.Now.Year - dateOfBirth.Year) > DateTime.Now)
+      {
+        age--;
+      }
+    }
+    // C#
+    void IDeserializationCallback.OnDeserialization(Object sender)
+    {
+      CalculateAge();
+      Console.WriteLine("calc");
+    }
+
+  }
+
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialize_People
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length == 0)
+      {
+        Person p = Deserialize();
+        Console.WriteLine(p.ToString());
+      }
+      else
+      {
+        try
+        {
+          if (args.Length != 4)
+          {
+            throw new ArgumentException("You must provide four arguments.");
+          }
+
+          DateTime dob = new DateTime(Int32.Parse(args[1]), Int32.Parse(args[2]), Int32.Parse(args[3]));
+          Person p = new Person(args[0], dob);
+          Console.WriteLine(p.ToString());
+
+          Serialize(p);
+        }
+        catch (Exception ex)
+        {
+          DisplayUsageInformation(ex.Message);
+        }
+      }
+    }
+
+    private static void DisplayUsageInformation(string message)
+    {
+      Console.WriteLine("\nERROR: Invalid parameters. " + message);
+      Console.WriteLine("\nSerialize_People \"Name\" Year Month Date");
+      Console.WriteLine("\nFor example:\nSerialize_People \"Tony\" 1922 11 22");
+      Console.WriteLine("\nOr, run the command with no arguments to display that previous person.");
+    }
+
+    private static void Serialize(Person sp)
+    {
+      FileStream fs = new FileStream("Person.Dat", FileMode.Create);
+
+      BinaryFormatter bf = new BinaryFormatter();
+
+      bf.Serialize(fs, sp);
+
+      fs.Close();
+
+    }
+
+    private static Person Deserialize()
+    {
+      Person dsp = new Person();
+      FileStream fs = new FileStream("Person.Dat", FileMode.Open);
+      BinaryFormatter bf = new BinaryFormatter();
+      dsp = (Person)bf.Deserialize(fs);
+
+      return dsp;
+
+    }
+  }
+}
