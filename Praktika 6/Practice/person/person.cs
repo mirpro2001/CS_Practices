b29344dca44bd6420e550833b95c81dc1c5f9127
mirpro2001 <70947376+mirpro2001@users.noreply.github.com>
2022-12-01 18:05:28 +0300

    Some fixes

---
+++ Praktika 6/Practice/person/person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace prakt6_var1
+{
+	public class person
+	{
+		private string person;
+		private string fio;
+		private string name;
+		private string surname;
+		private DateTime bdate;
+		public person(string _name, string _surname, DateTime _bdate)
+		{
+			name = _name;
+			surname = _surname;
+			bdate = _bdate;
+		}
+	}
+}
